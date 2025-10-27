b05ab48e1a590dc876fc91bdaea42e00d599fb17
Hardik Wahi <wahi.mani21@gmail.com>
2025-10-27 13:29:39 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3977006819-31b40",
+  "appId": "1:651206512593:web:4d205517fcd63211009af0",
+  "apiKey": "AIzaSyC9mM1p3ugl7TmjBFAvI70FzzQ2KqnMqkM",
+  "authDomain": "studio-3977006819-31b40.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "651206512593"
+};
