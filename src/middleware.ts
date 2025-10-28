2b25aeb9c2053e1ff100e12f6182f66e2136e455
Hardik Wahi <wahi.mani21@gmail.com>
2025-10-28 17:02:02 +0000

    Try fixing this error: `Build Error: Export default doesn't exist in tar

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,5 +10,5 @@ export default createMiddleware({
  
 export const config = {
   // Match only internationalized pathnames
-  matcher: ['/', '/(de|en|es|fr|hi)/:path*']
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
 };
