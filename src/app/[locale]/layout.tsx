428e36248f289b7f22a62a11d73aeee9e7f1d2d1
Hardik Wahi <wahi.mani21@gmail.com>
2025-10-30 19:39:30 +0000

    like see, what the fuxk is this?

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -31,7 +31,7 @@ export default async function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
       <body>
-        <NextIntlClientProvider messages={messages}>
+        <NextIntlClientProvider locale={locale} messages={messages}>
           <ThemeProvider
             attribute="class"
             defaultTheme="light"
